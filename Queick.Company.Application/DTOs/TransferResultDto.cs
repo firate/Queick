c11dd484a28d1e8123a6bf6430694c76a8dd1f67
Fırat Ergül <firatergul@windowslive.com>
2025-05-20 23:42:37 +0300

    daha sonra tekrar eklenecek. yoruma alındı.

--- Queick.Company.Application/DTOs/TransferResultDto.cs
+++ Queick.Company.Application/DTOs/TransferResultDto.cs
@@ -3,11 +3,11 @@ namespace Queick.Company.Application.DTOs;
 /// <summary>
 /// Transfer sonucu Data Transfer Object sınıfı
 /// </summary>
-public class TransferResultDto
-{
-    public bool Success { get; set; }
-    public long EmployeeId { get; set; }
-    public long SourceCompanyId { get; set; }
-    public long TargetCompanyId { get; set; }
-    public long TransferRecordId { get; set; }
-}
+// public class TransferResultDto
+// {
+//     public bool Success { get; set; }
+//     public long EmployeeId { get; set; }
+//     public long SourceCompanyId { get; set; }
+//     public long TargetCompanyId { get; set; }
+//     public long TransferRecordId { get; set; }
+// }
