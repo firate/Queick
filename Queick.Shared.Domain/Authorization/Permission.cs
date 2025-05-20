c642be7fa570165cb4a430019b85bb2ed0fb7f6a
Fırat Ergül <firatergul@windowslive.com>
2025-05-20 23:42:15 +0300

    authorization daha sonra tekrar eklenecek, kaldırıldı.

--- Queick.Shared.Domain/Authorization/Permission.cs
+++ Queick.Shared.Domain/Authorization/Permission.cs
@@ -1,27 +1,27 @@
 namespace Queick.Shared.Domain.Authorization;
 
-public sealed class Permission : ValueObject
-{
-    public string Name { get; }
-    
-    private Permission(string name)
-    {
-        Name = name;
-    }
-    
-    public static Permission Create(string name) => new(name);
-    
-    protected override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return Name;
-    }
-    
-    // Common Permission Constants
-    public static class Common
-    {
-        public static Permission Read => Create("read");
-        public static Permission Create => Create("create");
-        public static Permission Update => Create("update");
-        public static Permission Delete => Create("delete");
-    }
-}
+// public sealed class Permission : ValueObject
+// {
+//     public string Name { get; }
+//     
+//     private Permission(string name)
+//     {
+//         Name = name;
+//     }
+//     
+//     public static Permission Create(string name) => new(name);
+//     
+//     protected override IEnumerable<object> GetEqualityComponents()
+//     {
+//         yield return Name;
+//     }
+//     
+//     // Common Permission Constants
+//     public static class Common
+//     {
+//         public static Permission Read => Create("read");
+//         public static Permission Create => Create("create");
+//         public static Permission Update => Create("update");
+//         public static Permission Delete => Create("delete");
+//     }
+// }
