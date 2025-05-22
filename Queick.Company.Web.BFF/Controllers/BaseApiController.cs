609fa449383c2c3651256e11f1d56eeee6492880
Fırat Ergül <firatergul@windowslive.com>
2025-05-22 23:32:19 +0300

    BaseController oluşturuldu.

---
+++ Queick.Company.Web.BFF/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Queick.Company.Web.BFF.Controllers;
+
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public abstract class BaseApiController : ControllerBase
+{
+    
+}
